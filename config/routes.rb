f398c480775e28142ca79b78bdfa86c4f9f22cc8
Sam Devine <samuel.will.devine@gmail.com>
2021-11-10 10:18:39 -0700

    feat: garden show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :plots, only: [:index]
+  resources :gardens, only: [:show]
 
   delete "/plots/:plot_id/plants/:plant_id", to: 'plant_plots#destroy'
 end
