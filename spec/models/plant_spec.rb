2e4f3044621e796098e701c391abf69007d30427
Sam Devine <samuel.will.devine@gmail.com>
2021-11-10 08:21:09 -0700

    add relationships to models

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Plot do
+RSpec.describe Plant do
   describe 'relationships' do
     it { should have_many(:plant_plots) }
     it { should have_many(:plots).through(:plant_plots) }
