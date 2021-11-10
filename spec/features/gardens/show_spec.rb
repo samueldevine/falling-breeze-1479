d657eef0792cc0fb2d31843dc5e19d710e6b31d0
Sam Devine <samuel.will.devine@gmail.com>
2021-11-10 10:39:42 -0700

    extension complete, 100% coverage

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -27,9 +27,15 @@ RSpec.describe 'the garden show page' do
         description: "It's been dormant for eons.",
         days_to_harvest: 101
       )
+      @plant_4 = @plot_1.plants.create(
+        name: 'Pumpkin',
+        description: 'A fall favorite, grown for its crunchy seeds and delicately flavored flesh. As a bonus, the hollow shell can be carved into a festive decoration.',
+        days_to_harvest: 13
+      )
 
-      # add duplicate plants to test for uniqueness
-      @plot_2.plants << @plant_1
+      # add duplicate plants to test for uniqueness and ordering
+      @plot_2.plants << @plant_4
+      @plot_2.plants << @plant_2
       @plot_2.plants << @plant_2
     end
 
@@ -51,9 +57,15 @@ RSpec.describe 'the garden show page' do
 
       it 'only includes plants that take less than 100 days to harvest' do
         visit garden_path(@turing_garden.id)
-        save_and_open_page
 
         expect(page).to_not have_content @plant_3.name
+      end
+
+      it 'is ordered by the number of plants in the garden from most to least' do
+        visit garden_path(@turing_garden.id)
+
+        expect(@plant_2.name).to appear_before @plant_4.name
+        expect(@plant_4.name).to appear_before @plant_1.name
       end
     end
   end
